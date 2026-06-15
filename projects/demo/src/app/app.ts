0ef1a4e552abb092cc7775715b82c05fe1b5404d
Steffen Dielmann <steffen@dielmann.consulting>
2026-06-15 21:16:41 +0200

    feat: add demo application showcasing all six ng-anim8 components

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,11 +1,36 @@
 import { Component, signal } from '@angular/core';
+import {
+  CollapseComponent,
+  FadeComponent,
+  GrowComponent,
+  SlideComponent,
+  StaggerComponent,
+  ZoomComponent,
+} from 'ng-anim8';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [
+    CollapseComponent,
+    FadeComponent,
+    GrowComponent,
+    SlideComponent,
+    StaggerComponent,
+    ZoomComponent,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('demo');
+  fadeSig     = signal(false);
+  slideSig    = signal(false);
+  collapseSig = signal(false);
+  growSig     = signal(false);
+  zoomSig     = signal(false);
+  items       = signal<string[]>([]);
+
+  addItem(): void {
+    this.items.update((list) => [...list, `Item ${list.length + 1}`]);
+  }
 }
